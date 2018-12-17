a788341d16f846cbf983d0282164f63e81e5daaa
Daniel Toma <danieltoma@ubalumni.com>
2018-12-17 22:47:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   arr = []
-#  array.each { |item| arr << item ** 2 }
-#  arr
+   array.each { |item| arr << item ** 2 }
+   arr
 
 #  array.each |item| do
 #    arr << item ** 2
